ed8f876e1a7a15d449ce7c7d3d0d3e8ddfa322ab
Vish <savishy.pv@gmail.com>
2016-08-23 17:25:50 +0530

    working shell-script-based deployment - create and push a docker image - create google container cluster - use docker image to create kubernetes pods and deployment. - expose the public IP - display public IP

--- node-kube-docker-gce/run.rb
+++ node-kube-docker-gce/run.rb
@@ -4,19 +4,26 @@ require 'bundler/setup'
 require 'json'
 require 'common_utils'
 
-
-unless Common_utils.which('gcloud').nil?
-	system('gcloud')
-end
+# required commands
+['gcloud','docker','kubectl'].each { |x| 
+	if Common_utils.which(x).nil?
+		abort "command #{x} not found"
+	end
+}  
 
 jsonfile = File.read("config.json")
 json_data = JSON.parse(jsonfile)
-docker_login = "docker login -u " + json_data['docker_username'] + " -p " + json_data['docker_password']
-puts "-- logging in to Docker Hub (user #{json_data['docker_username']} )"
-system(docker_login)
+docker_username = json_data['docker_username']
+docker_password = json_data['docker_password']
+puts "-- logging in to Docker Hub (user #{docker_username} )"
+docker_login = "docker login -u #{docker_username} -p #{docker_password}"
+out = system(docker_login)
+if $?.exitstatus != 0
+	abort "Error: #{out}"
+end
 
 puts "-- building image #{json_data['docker_image_name']}"
-docker_image_name = "#{json_data['docker_username']}/#{json_data['docker_image_name']}:#{json_data['docker_image_version']}"
+docker_image_name = "#{docker_username}/#{json_data['docker_image_name']}:#{json_data['docker_image_version']}"
 
 docker_build = "docker build -t #{docker_image_name} node-application"
 puts docker_build
@@ -25,3 +32,25 @@ system(docker_build)
 puts "-- pushing image #{docker_image_name}"
 docker_push = "docker push #{docker_image_name}"
 system(docker_push)
+
+gce_cluster_name = json_data['gce_cluster_name']
+
+puts "-- creating google cloud container cluster #{gce_cluster_name}"
+system("gcloud config set project #{json_data['gce_project']}")
+system("gcloud config set compute/zone #{json_data['gce_compute_zone']}")
+system("gcloud container clusters create #{gce_cluster_name}")
+system("gcloud container clusters get-credentials #{gce_cluster_name}")
+
+# fix for problem finding kubeconfig on my windows machine
+# (my Windows machine has $HOME set. YMMV.)
+kubectl_cmd = "kubectl --kubeconfig=#{ENV['HOME']}\\.kube\\config"
+
+# Create a kubernetes pod and deployment.
+system("#{kubectl_cmd} run #{gce_cluster_name} --image=#{docker_image_name} --port=8080")
+
+# expose public IP address
+system("#{kubectl_cmd} expose deployment #{gce_cluster_name} --type=\"LoadBalancer\"")
+
+# print the exposed IP address
+echo "-- Exposed IP Address:"
+system("#{kubectl_cmd} get services kubecluster -o=\"jsonpath={.status.loadBalancer.ingress[].ip}\"")
